60417d25c8a52db7caae9f2fcbee77e6cbccc54d
Andrey Mazuro <andrey.mazuro@icloud.com>
2017-10-04 13:58:01 +0300

    added users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,13 @@
 class UsersController < ApplicationController
+
+  def create
+    @user = User.create(user_params)
+    head 200
+  end
+
+  private
+    def user_params
+      params.permit(:username, :password, :fullName, :avatarUrl)
+    end
+
 end
